3117208aa305de5dbb0e5e155960e8efbd33b063
Jovo <sunjkajovo@gmail.com>
2019-11-14 02:28:59 +0100

    Initial project

---
+++ src/test/java/com/jovo/ScienceCenter/ScienceCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovo.ScienceCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
